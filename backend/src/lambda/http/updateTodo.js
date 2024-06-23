c1b1cdd5df1a322179f5a61aed1760b6286cd96b
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-06-23 20:34:35 +0900

    feat: implement updateTodo

--- backend/src/lambda/http/updateTodo.js
+++ backend/src/lambda/http/updateTodo.js
@@ -1,7 +1,41 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo = JSON.parse(event.body)
-  
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import middy from "@middy/core";
+import httpCors from "@middy/http-cors";
+import httpErrorHandler from "@middy/http-error-handler";
+import { createLogger } from "../../utils/logger.mjs";
+
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB());
+
+const todosTable = process.env.TODOS_TABLE;
+
+const logger = createLogger("updateTodo");
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    httpCors({
+      credentials: true,
+    })
+  )
+  .handler(async (event) => {
+    const todoId = event.pathParameters.todoId;
+    const updatedTodo = JSON.parse(event.body);
+
+    logger.info(`Update todo: ${todoId}, done: ${event.body}`);
+
+    await dynamoDbClient.update({
+      TableName: todosTable,
+      Key: todoId,
+      UpdateExpression: "SET name = :name, dueDate = :dueDate, done = :done",
+      ExpressionAttributeValues: {
+        ":name": updatedTodo.name,
+        ":dueDate": updatedTodo.dueDate,
+        ":done": updatedTodo.done,
+      },
+    });
+
+    return {
+      statusCode: 204,
+    };
+  });
