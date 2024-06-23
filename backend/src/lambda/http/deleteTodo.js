18f885e62c4ac6136913206dd34bc37bba93c8ee
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-06-23 20:59:55 +0900

    feat: implement deleteTodo

--- backend/src/lambda/http/deleteTodo.js
+++ backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,39 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import middy from "@middy/core";
+import httpCors from "@middy/http-cors";
+import httpErrorHandler from "@middy/http-error-handler";
+import { createLogger } from "../../utils/logger.mjs";
+import { getUserId } from "../utils.mjs";
 
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB());
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+const todosTable = process.env.TODOS_TABLE;
 
+const logger = createLogger("deleteTodo");
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    httpCors({
+      credentials: true,
+    })
+  )
+  .handler(async (event) => {
+    const userId = getUserId(event);
+    const todoId = event.pathParameters.todoId;
+
+    logger.info(`Delete todo ${todoId} from user ${userId}`);
+
+    await dynamoDbClient.delete({
+      TableName: todosTable,
+      Key: {
+        userId,
+        todoId,
+      },
+    });
+
+    return {
+      statusCode: 204,
+    };
+  });
