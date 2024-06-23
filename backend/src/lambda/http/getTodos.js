a18a827059590a4a3703d77c6550e1c64df9124a
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-06-23 19:27:41 +0900

    feat: add logger to getTodos

--- backend/src/lambda/http/getTodos.js
+++ backend/src/lambda/http/getTodos.js
@@ -3,12 +3,15 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import middy from "@middy/core";
 import httpCors from "@middy/http-cors";
 import httpErrorHandler from "@middy/http-error-handler";
+import { createLogger } from "../../utils/logger.mjs";
 import { getUserId } from "../utils.mjs";
 
 const dynamoDbClient = DynamoDBDocument.from(new DynamoDB());
 
 const todosTable = process.env.TODOS_TABLE;
 const todosUserIdIndex = process.env.TODOS_USER_ID_INDEX;
+
+const logger = createLogger("getTodos");
 
 export const handler = middy()
   .use(httpErrorHandler())
@@ -19,6 +22,8 @@ export const handler = middy()
   )
   .handler(async (event) => {
     const userId = getUserId(event);
+
+    logger.info(`Get all todos for user: ${userId}`);
 
     const result = await dynamoDbClient.query({
       TableName: todosTable,
