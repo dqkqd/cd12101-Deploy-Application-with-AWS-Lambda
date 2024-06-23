4fd369c86dcb851ca4e73fdf598e63e1a1a8a440
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-06-23 21:45:37 +0900

    feat: implement generateUploadUrl

--- backend/src/lambda/http/generateUploadUrl.js
+++ backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,42 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import middy from "@middy/core";
+import httpCors from "@middy/http-cors";
+import httpErrorHandler from "@middy/http-error-handler";
+import { createLogger } from "../../utils/logger.mjs";
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+const s3Client = new S3Client();
 
+const bucketName = process.env.TODOS_IMAGE_S3_BUCKET;
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION);
+
+const logger = createLogger("generateUploadUrl");
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    httpCors({
+      credentials: true,
+    })
+  )
+  .handler(async (event) => {
+    const todoId = event.pathParameters.todoId;
+
+    const command = new PutObjectCommand({
+      Bucket: bucketName,
+      Key: todoId,
+    });
+
+    const url = await getSignedUrl(s3Client, command, {
+      expiresIn: urlExpiration,
+    });
+
+    logger.info(`Created url for todo ${todoId}: ${url}`);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        uploadUrl: url,
+      }),
+    };
+  });
