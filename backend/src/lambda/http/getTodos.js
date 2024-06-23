4489cbd5c4ab3a6bb4d4516236656b7203815432
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-06-23 18:58:39 +0900

    feat: add TodosTable and implement getTodos

--- backend/src/lambda/http/getTodos.js
+++ backend/src/lambda/http/getTodos.js
@@ -1,4 +1,37 @@
-export function handler(event) {
-  // TODO: Get all TODO items for a current user
-  return undefined
-}
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import middy from "@middy/core";
+import httpCors from "@middy/http-cors";
+import httpErrorHandler from "@middy/http-error-handler";
+import { getUserId } from "../utils.mjs";
+
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB());
+
+const todosTable = process.env.TODOS_TABLE;
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    httpCors({
+      credentials: true,
+    })
+  )
+  .handler(async (event) => {
+    const userId = getUserId(event);
+
+    const result = await dynamoDbClient.query({
+      TableName: todosTable,
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+      },
+    });
+    const items = result.Items;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items,
+      }),
+    };
+  });
